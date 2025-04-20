a26061dfdeed8954a51a7dce417ef59c4aeca805
koko812 <iwakawa.koichi.q5@dc.tohoku.ac.jp>
2025-04-20 20:33:40 +0900

    esa_extend

--- main.js
+++ main.js
@@ -8,8 +8,9 @@ let gameover = false
 const size = 300
 const initiallength = 200
 const snakePositionList = []
+const extentionLength = 50 
 const snakeWidth = 5
-const snakeSpeed = 2
+const snakeSpeed = 1
 let angle = -90
 let move = 0
 
@@ -73,6 +74,31 @@ const collisionCheck = () => {
     const tx = mx + Math.cos(angle / 180 * Math.PI) * (snakeWidth + 2)
     const ty = my + Math.sin(angle / 180 * Math.PI) * (snakeWidth + 2)
     const pix = data[Math.trunc(tx) * 4 + Math.trunc(ty) * 4 * size + 1]
+
+    // mx がいまだに蛇のどこの座標なのかが曖昧だというアホっぷり
+    if((mx -fx)**2+(my-fy)**2 < (snakeWidth*2+4)**2){
+        fx = 0;
+        // うまくは動いてるんだけど，なんでうまくいってるのかがわからない
+        // けつにまるをどんどん追加していってるんだけど，それがなんで綺麗に追従してくれるのかがわからない
+        // update の部分で，マイループ進む距離分を足し算してる処理が入ってる
+        // まあつまりその処理のおかげで結構うまくいってるとかいう話だと思う
+        // ループごとに頭を足して，お尻を消してってやってるのが大事っぽい，それで納得だね
+        // 実際動いてるのは頭とお尻だけというのがわかっていれば，納得できるはず
+
+        // あとまあ関係ないけど，キーボード操作は可能にしておきたいかもしれないという話
+        // これもう最終的には，画面を埋め尽くすようにグネグネ動いて，
+        // 一周するごとに餌をとっていくとかそういう感じになるのかもしれないな
+        // 是非とも ai にやらせてみたいところ，javascript との連携をそう簡単にできるのかって話だけど
+    
+        // あとは色の変化なんだけど，そこの処理が普通にややこしそうでちょっとだけ萎えてる
+        // 多分 render のところをいじるだけって話なんだろうけどね
+
+        const lastPos = snakePositionList[0]
+        for(let i = 0; i < extentionLength; i++){
+            snakePositionList.unshift(lastPos)
+        }
+        return
+    }
 
     if (mx - snakeWidth < 0 || size < snakeWidth + mx || my - snakeWidth < 0 || size < snakeWidth + my) {
         return true
